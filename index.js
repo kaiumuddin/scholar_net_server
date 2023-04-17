1f586214bfb304de52e7a7284ddefa37e7f69c16
kaiumuddin <kaium.uddin2909@gmail.com>
2023-04-17 12:55:05 +0600

    Refreshed

--- index.js
+++ index.js
@@ -16,74 +16,9 @@ const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: 
 async function run() {
     try {
         const postCollection = client.db('todoDB').collection('todos');
-        // const todo = {text: "Hello World", isDone: false};
-
-        // app.get('/', (req, res) => {
-        //     res.send('Simple Node server running');
-        // });
-
-        // app.get('/todos', async (req, res) => {
-        //     const cursor = todoCollection.find({});
-
-        //     const todos = await cursor.toArray();
-
-        //     res.send(todos);
-        // });
-
-        // app.post('/todos', async (req, res) => {
-        //     console.log('Post Api Called');
-
-        //     const todo = req.body;
-        //     const result = await todoCollection.insertOne(todo);
-
-        //     todo._id = result.insertedId;
-
-        //     console.log(result);
-        //     res.send(todo);
-        // });
-
-        // app.delete('/todos/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     console.log(id);
-        //     const filter = {_id: new ObjectId(id)};
-        //     const result = await todoCollection.deleteOne(filter);
-        //     res.send(result);
-        // });
-
-        // app.put('/todos/:id', async (req, res) => {
-        //     const isDone = req.body.isDone;
-        //     console.log(isDone);
 
 
-        //     const id = req.params.id;
-        //     const filter = {_id: new ObjectId(id)};
-        //     const options = {upsert: true};
-        //     const updatedDoc = {
-        //         $set: {
-        //             isDone: isDone,
-        //         }
-        //     };
 
-        //     const result = await todoCollection.updateOne(filter, updatedDoc, options);
-        //     res.send(result);
-        // });
-
-        // app.put('/edit/:id', async (req, res) => {
-        //     const newText = req.body.newText;
-        //     console.log(newText);
-
-        //     const id = req.params.id;
-        //     const filter = {_id: new ObjectId(id)};
-        //     const options = {upsert: true};
-        //     const updatedDoc = {
-        //         $set: {
-        //             text: newText,
-        //         }
-        //     };
-
-        //     const result = await todoCollection.updateOne(filter, updatedDoc, options);
-        //     res.send(result);
-        // });
     }
     finally {
 
